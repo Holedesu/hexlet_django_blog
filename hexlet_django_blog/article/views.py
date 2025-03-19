54ccd27346321631bfb82b23eeb36ab965ae0ce7
Holedesu <88774183+Holedesu@users.noreply.github.com>
2025-03-19 14:29:41 +0300

    Выполнил задания из пятого урока

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -4,4 +4,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('article')
+    name, course, group = "Иннокентий", "3", "3103-О"
+    return render(
+        request,
+        'articles/index.html',
+        context={'name': name,
+                 'course': course,
+                 'group': group},
+    )
